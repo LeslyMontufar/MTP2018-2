45c53dc85d3b7256dcca5e0c6c6a903901e2e873
LeslyMontufar <39318769+LeslyMontufar@users.noreply.github.com>
2018-12-13 01:19:34 +0000

    ok

--- P6.c
+++ P6.c
@@ -1,10 +1,11 @@
 /* P6.c */
 /* Lesly Viviane Montúfar Berrios */
 /* 11811ETE001 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define pi 3.14159265359
+//#define pi 3.14159265359 //tem o M_PI
 #define r 1
 
 typedef struct {
@@ -12,17 +13,23 @@ typedef struct {
 }Ponto;
 
 int main(){
-   int N, i; //stack memory
-   Ponto *xy;
-   scanf("%i", &N);getchar();
-   xy= (Ponto *) malloc(N*sizeof(Ponto)); //heap memory
-   printf("%i retorna \" ",N);
-   for(i=0; i<N; i++){
-     xy->x= r*cos(i*2*pi/(N));
-     xy->y= r*sin(i*2*pi/N);
-     printf("(%.3f, %.3f) ", xy->x, xy->y);
-   }
-   printf("\" ");
-   free(xy);
+	Ponto *p;
+	int N, i, nCol=2;
+
+	fprintf(stdout, "\n\n\tN: ");
+	fscanf(stdin, "%i", &N);getchar();
+
+	p = (Ponto *) malloc(N*sizeof(Ponto));
+
+	fprintf(stdout, "\n\n\t\"%i\" retorna \" ",N);
+	for(i=0; i<N; i++){
+	   	p[i].x= r*cos(i*2*M_PI/(N-1));
+	   	p[i].y= r*sin(i*2*M_PI/(N-1));
+	   	fprintf(stdout, "(%.3f, %.3f) ", p[i].x, p[i].y);
+	   	if((i+1)%nCol==0 && i!=N-1){
+	   		fprintf(stdout, "\n\t");
+	   	} 		
+	}
+   fprintf(stdout, "\"\n");	
    return 0;
 }
